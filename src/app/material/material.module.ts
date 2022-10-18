d55f805a237e6050ac1c31158a093d16d57f84ed
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-18 15:50:06 -0300

    V186 - Sidenav - Color icon

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon'; 
+import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
@@ -11,6 +12,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
  exports: [
   MatButtonModule,
   MatIconModule,
+  MatListModule,
   MatSidenavModule,
   MatToolbarModule
  ]
