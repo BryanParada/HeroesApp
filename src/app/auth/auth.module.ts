b2f136ecb09e8ea8fc9553f44c03e6b4f8e30604
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-18 12:11:39 -0300

    V176 - LazyLoad - Child Routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
@@ -12,7 +13,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
     RegistroComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
