53667105a151eea2ada3b041cdb0118fd419a403
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-18 17:52:13 -0300

    V190-191 - Flex Layout - Show IMG

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,7 +5,12 @@ import { Hero } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
+  styles: [`
+    mat-card {
+      margin-top: 20px;
+      
+    }
+  `
   ]
 })
 export class ListadoComponent implements OnInit {
