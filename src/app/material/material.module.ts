d73aff7aff0ed60d2e3707c0cf1bd2c22be096b2
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-19 17:31:24 -0300

    V207 - Add hero page - Select Material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatListModule} from '@angular/material/list'; 
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import {MatSelectModule} from '@angular/material/select';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
@@ -24,6 +25,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   MatGridListModule,
   MatListModule,
   MatProgressSpinnerModule,
+  MatSelectModule,
   MatSidenavModule,
   MatToolbarModule
  ]
