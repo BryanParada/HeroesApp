e1d748a7266d56b9d52a39799e6e48cfcfba7189
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-18 22:05:28 -0300

    V194 - RouterLink - ActivatedRoute

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+
+  id: string = '';
 
   ngOnInit(): void {
-  }
+ 
+    this.route.params
+    .subscribe( ({ id }) => console.log( id ));
+
+    // this.route.params
+    // .subscribe(
+    //   (params: Params) => { 
+    //     console.log(params["id"]);
+    //   });
+      
+    
+    }
+  
 
 }
