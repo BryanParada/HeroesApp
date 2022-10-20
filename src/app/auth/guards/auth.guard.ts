c294ea9f76e801a1d4f2c209a58c903f5dc6ea5d
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-20 14:28:37 -0300

    V224 - CanLoad

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+
+constructor( private authService: AuthService){}
+
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+
+      if(this.authService.auth.id)
+      {
+        return true;
+      } 
+      else
+      {
+        console.log('Bloqueado por el Authguard'); 
+        return false;
+      }
+
+      // console.log('can load',false);      
+      // console.log(route);
+      // console.log(segments);
+
+
+      
+  }
+}
