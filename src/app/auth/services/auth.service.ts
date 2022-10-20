6381d521685aa1e220b008b9b3d743501a816f97
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-20 13:58:33 -0300

    V223 - Tap-DisplayUsername

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,23 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseURL:String = environment.ApiEndpoint;
+  private _auth: Auth | undefined;
 
+  get auth(): Auth{
+    return {...this._auth!}
+  }
 
   constructor( private http:HttpClient,
                ) { }
 
   loginService(){
-      return this.http.get<Auth>(`${this.baseURL}/usuarios/1`);
+      return this.http.get<Auth>(`${this.baseURL}/usuarios/1`)
+            .pipe(
+              //para generar efectos secundarios cuando se realice la peticion, y antes de
+              //llegar al subscribe de login.component
+              // pasara primero por el sgte tap.
+              //informacion de backend se almacenara en variable de tipo interface
+              tap(   auth => this._auth = auth   )
+            );
   }
 }
